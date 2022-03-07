c08c1cf0e78ff3f898fbc3eeaf73efc743ed43b3
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-03-08 00:23:41 +0100

    feat: lab_3

---
+++ lab_3/laravapi/app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return $user;
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+        $user = User::where('email', $fields['email'])->first(); // sprawdzenie maila
+        if (!$user || !Hash::check($fields['password'], $user->password)) { // sprawdzenie hasła
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+        $token = $user->createToken('myapptoketn')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'Logged out'
+        ];
+    }
+}
