5cdc827eb81ab32ca13f8c36f016e73a909a614d
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-03-31 14:06:55 +0200

    feat: lab_1b finished

--- lab_1b/zad_1_19/index.js
+++ lab_1b/zad_1_19/index.js
@@ -1,11 +1,33 @@
 const express = require('express')
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+
 const app = express()
 const port = 3000
 
+// Creating session
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized: true,
+    cookie: {maxAge: oneDay},
+    resave: false
+}));
+
+// cookie parser middleware
+app.use(cookieParser());
+
+const visitCountMap = new Map();
 
 app.get('/', (req, res) => {
-    const sessionId = req.cookies.sessionId;
-    console.log(sessionId);
+    const sessionId = req.sessionID;
+    if (visitCountMap.has(sessionId)) {
+        visitCountMap.set(sessionId, visitCountMap.get(sessionId) + 1);
+    } else {
+        visitCountMap.set(sessionId, 1);
+    }
+
+    res.send(`Odwiedziłeś stronę: ${visitCountMap.get(sessionId)} razy`);
 })
 
 app.listen(port, () => {
