56075179df8bf2a00c372bb7af62c2a3968f71a2
lavertis <29688908+Lavertis@users.noreply.github.com>
2022-05-04 15:32:04 +0200

    feat: task 5

---
+++ lab_2/task_5/MathOperationsModuleExport.js
@@ -0,0 +1,22 @@
+export const calc = (a, b, p) => {
+    if (!(a || b || p)) {
+        return 'Please enter all of the arguments';
+    }
+    switch (p) {
+        case '+': {
+            return a + b
+        }
+        case '-': {
+            return a - b
+        }
+        case '*': {
+            return a * b
+        }
+        case '/': {
+            return a / b
+        }
+        default: {
+            return
+        }
+    }
+}
